452c839f6b9cf226250266491a38915eae8d884f
Austin Inmon <ainmon987@west-mec.org>
2019-04-09 22:16:33 -0700

    final commit
    Added comments.Commit message from earlier today  -Added the new web table. Added style rules to the new style sheet. Added ascending and descending arrow statements.   populate the tableData array as a two-dimensional array with values taken from rows and cells of the web table body.  create the table body based on the sorted data.set up the appearance and behavior of the column headings.   Ready for review.

--- gi_sort .js
+++ gi_sort .js
@@ -51,39 +51,42 @@
     
 
 */
-
+//Store data in the web table 
 var tableData = [];
-
+//store the text of the column headings
 var dataCategories = [];
-
+//first table column
 var sortIndex = 0;
-
+//whether the column is aceding or decending.
 var sortDirection = 1;
-
+//anyomous funtion that is calling the different functions.
 window.addEventListener("load", function () {
       defineDataArray();
       tableData();
       defineColumns();
 })
-
+      //populate the tableData array as a two-dimensional array with values taken from rows and cells of the web table body.
 function defineDataArray() {
+      //getting all content from the html file that has the selector.
       var tableRows = document.querySelectorAll("table.sortable tbody tr");
-
+      //looping through table rows.
       for (var i = 0; i < tableRows.length; i++) {
-
+      //putting rows in row cells. 
             var rowCells = tableRows[i].children;
-
+      //creating an new array.
             var rowValues = new Array(rowCells.length);
-
+      //looping theough the content of thew rowcells var. 
             for (var a = 0; a < rowCells.length; a++) {
                   rowValues[a] = rowCells[a].textContent;
 
             }
-            tableData.push(rowValues);
+      
+      tableData.push(rowValues);
       }
-      tableData[i].sort(dataSort2D);
+      
+      tableData.sort(dataSort2D);
 }
-
+//create the table body based on the sorted data.
 function writeTableData() {
       var newTableBody = document.createElement("tbody");
 
@@ -93,33 +96,32 @@ function writeTableData() {
       }
 
 }
-
-
-
+//set up the appearence and behavior of the column headings.
 function defineColumns() {
+      //Creating a style page and adding it to the html document. 
       var embedStyle = document.createElement("style");
-
+      //Puting the stylesheet in the html head.
       document.head.appendChild(embedStyle);
-
+      //Adding a new style to the embed style page
       embedStyle.instertRule(
             "table.sortable thead tr th {\
                   cursor: pointer;\
-            }");
-
+            }", 0);
+      //Adding a new style to the embed style page
       embedStyle.insertRule(
             "table.sortable thead tr th: :after {\
                   content: '\\0a0';\
                   font-family: monospace;\
                   margin-left: 5px;\
-            }");
-
+            }", 1);
+      //Adding a new style to the embed style page
       embedStyle.insertRule(
             "table.sortable thead tr th:nth-of-type()1::after{\
                   content: '\\25b2';\
-            }");
-
+            }", 2);
+      //getting all content from the html file that has the selector.
       var theads = document.querySelectorAll("table.sortable thead th");
-
+      //looping throught the th elements in the table headings. 
       for (var i = 0; i < theads.length; i++) {
             var text = theads[i].innerHTML;
 
@@ -129,35 +131,39 @@ function defineColumns() {
 
       }
 }
-
-function columnSort(e) {
+      //sort the web table based on the column heading that the user clicked.
+      function columnSort(e) {
+      //determines which column is clicked.
       var columnText = e.target.textcontent;
-
+      //Retreive the index number of the column.
       var columnIndex = dataCategories[i].indexOf(columnText);
-
+      //testing to see if columnindex is the same thing as sortIndex.
       if (columnIndex === sortIndex) {
-            sortDirection *= -1;
+          sortDirection *= -1;
+      //sortindex is equal to columnindex. 
       } else {
             sortIndex = columnIndex;
       }
-
+      //move he icon into the column heading cell.
       var columnNumber = columnIndex + 1;
-
+      //delelte the last style rule that is in the embedstyle stylesheet.
       var columnStyles = document.styleSheets[document.styleSheets.length - 1];
-
+      //displaying up arrow to show ascending.
       if (sortDirection === 1) {
             columnStyles.insertRule(
                   "table.sortable thead tr th:nth-of-type" + columnNumber + "::after {\
                         content: '\\25b2';\
-                  }");
+                  }", 1);
+      //displaying down arrow to show descending.
       } else {
             columnStyles.insertRule(
                   "table.sortable thead tr th:nth-of-type" + columnNumber + "::after {\
                         content: '\\25bc';\
-                  }");
+                  }", 1);
       }
+      //using thedataSort2D as the compared function.
       tableData.sort(dataSort2D);
-
+//Calling the writeTableData function. 
       writeTableData();
 }
 
